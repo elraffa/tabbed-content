1ac667de2af10cc40d1d460f095e1ee7a89d918b
federicoruffa <federico@inboundav.com>
2025-07-14 19:28:25 -0300

    change version

--- tabbed-content.php
+++ tabbed-content.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Tabbed Content
  * Description:       A custom block for displaying tabbed content.
- * Version:           0.1.0
+ * Version:           0.1.1
  * Requires at least: 6.7
  * Requires PHP:      7.4
  * Author:            Federico Ruffa
